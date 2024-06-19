45f7f41c3cd115ab09042c6e1c941b85f11878f1
Synapsbum <emreemre2@icloud.com>
2024-06-19 16:54:02 +0300

    ぬ
    ぬ

---
+++ server/src/clean.bat
@@ -0,0 +1,16 @@
+del /s "%~dp0\*.obj"
+del /s "%~dp0\*.ncb"
+del /s "%~dp0\*.ilk"
+del /s "%~dp0\*.idb"
+del /s "%~dp0\*.bsc"
+del /s "%~dp0\*.sbr"
+del /s "%~dp0\*.pch"
+del /s "%~dp0\*.pdb"
+del /s "%~dp0\*.user"
+del /s "%~dp0\*.ipch"
+del /s "%~dp0\*.sdf"
+del /s "%~dp0\*.tlog"
+del /s /q "%~dp0\deps"
+del /s /q "%~dp0\ipch"
+
+pause
